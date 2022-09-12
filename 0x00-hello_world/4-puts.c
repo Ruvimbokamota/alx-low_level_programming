b575edfa34e4f4865171908a8435b6b5937770f2
Ruvimbo Kamota <ruekamota@gmail.com>
2022-09-12 04:00:38 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
+
 /**
  *
- *  main - Main function of my project
+ * main - Main function of my project
  *
  *   
  *
  *    
  *
- *    Return: return 0 everytime
+ * Return: return 0 everytime
  *
  *
  */
+
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
