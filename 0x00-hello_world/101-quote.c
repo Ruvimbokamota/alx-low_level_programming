1b01c4eae0d0703acb0babb53e1108f81048accd
Ruvimbo Kamota <ruekamota@gmail.com>
2022-09-14 05:01:57 -0700

    adding files

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,22 @@
-#include <stdio.h>
+#include <unistd.h>
 
-/*main - main function 
+/**
  *
- * Write to stdout
- *Return: return 1
- */
-int main(void)
-{
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+ *   main - main function
 
-		return (1);
+ *
+ *    * Write to stdout
+ *
+ *     * Return: return 1
+ *
+ *      */
+
+int main(void)
+
+{
+
+		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+			return (1);
+
 }
