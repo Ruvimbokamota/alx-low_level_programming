fb995c50300f02348966b0cf17f3cb78d6c70230
Ruvimbo Kamota <ruekamota@gmail.com>
2022-09-15 18:48:56 -0700

    lowercase character

--- 0x04-more_functions_netsed_loops/0-isupper.c
+++ 0x04-more_functions_netsed_loops/0-isupper.c
@@ -1,13 +1,17 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - fuction that verifies if a character is uppercase or not
+ *
+ * @c: tested character
+ * Retunr: 1 if it is uppercase or 0 if it is not
+ */
 
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-	{
+	if((c >= 'A') && (c <= 'Z'))
 		return (1);
-	}
-	else
-       	{
+	
+
 		return (0);
-	}
 }
