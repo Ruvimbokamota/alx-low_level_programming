16f33b4c0ef275445d41e5d4856b700254b9549c
Ruvimbo Kamota <ruekamota@gmail.com>
2022-09-15 19:15:25 -0700

    l

--- 0x04-more_functions_netsed_loops/0-isupper.c
+++ 0x04-more_functions_netsed_loops/0-isupper.c
@@ -4,10 +4,10 @@
  * _isupper - fuction that verifies if a character is uppercase or not
  *
  * @c: tested character
- * Retunr: 1 if it is uppercase or 0 if it is not
+ * Return: 1 if it is uppercase or 0 if it is not
  */
 
-int _isupper(int c)
+int _isupper (int c)
 {
 	if ((c >= 'A') && (c <= 'Z'))
 	return (1);
