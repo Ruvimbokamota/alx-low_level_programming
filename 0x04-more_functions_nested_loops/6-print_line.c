caf2f670409a1b87a29d915e8be2bed8efc2ae76
Ruvimbo Kamota <ruekamota@gmail.com>
2022-09-16 17:55:04 -0700

    print straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - Prints a straight line
+ *
+ * @n:parameter
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	while (n-- > 0)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
