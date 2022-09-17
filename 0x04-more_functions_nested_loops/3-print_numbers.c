85c1ae6e3730a3a006d66157224469727a572192
Ruvimbo Kamota <ruekamota@gmail.com>
2022-09-16 17:40:45 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Function that print all numbers from 0 to 9
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int number = 48;
+
+	for (number = 48; number < 58; number++)
+	{
+		_putchar(number);
+	}
+	_putchar(10);
+}
