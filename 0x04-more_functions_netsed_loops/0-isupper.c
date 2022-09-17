f2eb76e1d052300eec1ed3d3dfb4d891857b2c41
Ruvimbo Kamota <ruekamota@gmail.com>
2022-09-16 17:20:36 -0700

    supper

--- 0x04-more_functions_netsed_loops/0-isupper.c
+++ 0x04-more_functions_netsed_loops/0-isupper.c
@@ -6,9 +6,11 @@
  * @c: tested character
  * Return: 1 if it is uppercase or 0 if it is not
  */
+
 int _isupper(int c)
 {
 	if ((c >= 'A') && (c <= 'Z'))
-	return (1);
+		return (1);
+
 	return (0);
 }
