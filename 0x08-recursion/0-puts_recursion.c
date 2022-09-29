bcaf64a75379cadac684916467c8a941e493c85c
Ruvimbo Kamota <ruekamota@gmail.com>
2022-09-29 05:07:59 -0700

    prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: pointer block of memory to fill
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		if (*s == '\0')
+			return;
+	}
+
+	_putchar (*s);
+	_puts_recursion(s + 1);
+}
