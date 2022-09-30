4d751dc82326245f7a8a089ee322d6b834fd1c60
Ruvimbo Kamota <ruekamota@gmail.com>
2022-09-30 04:37:24 -0700

    number of arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of the origram followed by \n
+ * argc:argument that counts argument input
+ * @argv: argument that stores the strings in an array of char* (strings)
+ * Return: 0
+ */
+int main(__attribute__((unused)) int argc, char **argv)
+{
+
+		printf("%s\n", argv[0]);
+
+		return (0);
+}
