876488937e13f98e9a5331ec0343eead7a8a0f50
Ruvimbo Kamota <ruekamota@gmail.com>
2022-10-12 15:42:58 -0700

    created a number of programs based on funtion pointers

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib>
+
+/**
+ * main - a program that prints the opcodes of its own main function.
+ * @argc: Arguments list.
+ * @argv: Arguments values.
+ * Return: Integer.
+ */
+
+int main(int argc, char *argv[])
+{
+	unsigned char *func_ptr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	int i;
+
+	for (i = 0; i < atoi(argv[1]); i++)
+	{
+		printf("%02hhx", *((char *)main + i));
+		if (i < atoi(argv[1]) - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
+
+	return (0);
+}
