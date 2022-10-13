a208a8760ee3988732756d2cc018e01ca119247b
Ruvimbo Kamota <ruekamota@gmail.com>
2022-10-13 07:46:49 -0700

    correction

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -23,21 +23,21 @@ void print_all(const char *const format, ...)
 
 		switch (format[x])
 		{
-			case 'c':
-				printf("%c", va_arg(valist, int));
-				break;
-			case 'i':
-				printf("%i", va_arg(valist, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(valist, double));
-				break;
-			case 's':
-				tmp_arg = va_arg(valist, char *);
-				if (tmp_arg == NULL)
-					tmp_arg = "(nil)";
-				printf("%s", tmp_arg);
-				break;
+		case 'c':
+			printf("%c", va_arg(valist, int));
+			break;
+		case 'i':
+			printf("%i", va_arg(valist, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(valist, double));
+			break;
+		case 's':
+			tmp_arg = va_arg(valist, char *);
+			if (tmp_arg == NULL)
+				tmp_arg = "(nil)";
+			printf("%s", tmp_arg);
+			break;
 		}
 		if ((format[x] == 'c' || format[x] == 'i' || format[x] == 's' ||
 			format[x] == 'f') && (format[x + 1] != '\0'))
