b971e4556cf54566ac0981de6a12d155b34ef913
Ruvimbo Kamota <ruekamota@gmail.com>
2022-10-20 07:26:46 -0700

    programmin- Singly linked lists on C

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,11 +1,13 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
+
 /**
  * print_all -  a function that prints anything (any arguement).
  * @format: Format of the arguments.
  * Return: void.
  */
+
 void print_all(const char *const format, ...)
 {
 	va_list valist;
@@ -23,21 +25,21 @@ void print_all(const char *const format, ...)
 
 		switch (format[x])
 		{
-		case 'c':
-			printf("%c", va_arg(valist, int));
-			break;
-		case 'i':
-			printf("%i", va_arg(valist, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(valist, double));
-			break;
-		case 's':
-			tmp_arg = va_arg(valist, char *);
-			if (tmp_arg == NULL)
-				tmp_arg = "(nil)";
-			printf("%s", tmp_arg);
-			break;
+			case 'c':
+				printf("%c", va_arg(valist, int));
+				break;
+			case 'i':
+				printf("%i", va_arg(valist, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(valist, double));
+				break;
+			case 's':
+				tmp_arg = va_arg(valist, char *);
+				if (tmp_arg == NULL)
+					tmp_arg = "(nil)";
+				printf("%s", tmp_arg);
+				break;
 		}
 		if ((format[x] == 'c' || format[x] == 'i' || format[x] == 's' ||
 			format[x] == 'f') && (format[x + 1] != '\0'))
